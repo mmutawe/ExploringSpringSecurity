634f7f7f3adac3ae9bcd7370eec09b2f34b71020
mmutawe <mahmoudmutawe@gmail.com>
2022-02-08 13:28:03 -0600

    Initial commit

---
+++ src/test/java/com/mmutawe/explore/spring/security/exploringspringsecurity/ExploringSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmutawe.explore.spring.security.exploringspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploringSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
